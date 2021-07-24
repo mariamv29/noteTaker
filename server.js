4dfbdc3d53af54841344c4d323262fad88d94ef3
Maria Mireles <mireles335@gmail.com>
2021-07-24 13:53:57 -0500

    api/post request works

--- server.js
+++ server.js
@@ -12,27 +12,29 @@ const notes = require("./Develop/db/db.json");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("public"));
+
 function createNewNote(body, notesArray) {
   const note = body;
   notesArray.push(note);
   fs.writeFileSync(
     path.join(__dirname, "./Develop/db/db.json"),
-    JSON.stringify({ notes: notesArray }, null, 2)
+    JSON.stringify(notesArray, null, 2)
   );
   return note;
 }
 
 function validateNote(note) {
-  if (!note.tile || typeof note.title !== "string") {
+  if (note.title === "") {
+    console.log(note);
+    console.log(note.title);
     return false;
   }
-  if (!note.text || typeof note.text !== "string") {
+  if (note.text === "") {
+    console.log(note.text);
     return false;
   }
-  if (!Array.isArray(notesArray)) notesArray = [];
-  {
-    return false;
-  }
+  return true;
 }
 
 app.get("/api/notes", (req, res) => {
@@ -43,17 +45,24 @@ app.get("/api/notes", (req, res) => {
 app.post("/api/notes", (req, res) => {
   //req.body is where incoming content will be
   console.log(req.body);
-  req.body.text = notes.length.toString();
-  
+  req.body.id = notes.length.toString();
+  console.log(req.body.text);
   // if any data is entered incorrect, send 400 error back
   if (!validateNote(req.body)) {
-      res.status(400).send("The note is not properly formatted.");
+    res.status(400).send("The note is not properly formatted.");
   } else {
-  // add note to json file and notes array in this function
-  const note = createNewNote(req.body, notes);
+    const note = createNewNote(req.body, notes);
 
-  res.json(note);
+    res.json(note);
   }
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
 });
 
 app.listen(3001, () => {
